4bb94c02d63839ecc86886ef4ebf65194d3d09d9
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-10 17:11:48 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,18 +1,22 @@
 add_rules("mode.debug", "mode.release")
 
-set_languages("c11", "cxx20")
+set_languages("c11", "cxx17")
 
 if (is_os("windows")) then 
-
+target("meta")
+    set_runtimes("MD")
+    set_kind("binary")
+    add_files("**.cpp")
+    add_cxflags("-Wno-c++11-narrowing")
+    add_links("lib/**")
+    add_links("Version", "advapi32", "Shcore", "user32", "shell32", "Ole32", {public = true})
 else
 
 add_requires("brew::llvm", {alias = "llvm"})
-add_requires("brew::zstd", {alias = "zstd"})
-add_requires("brew::ncurses", {alias = "ncurses"})
 
-target("SkrReflector")
+target("meta")
     set_kind("binary")
     add_files("**.cpp")
     add_packages("llvm", "zstd", "ncurses")
-    add_syslinks("pthread")
+    add_syslinks("pthread", "curses", "z", "dl", "m", "rt", "tinfo", "zstd")
 end
