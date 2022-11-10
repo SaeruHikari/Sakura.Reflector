8f09bdd6689a61cc5dbdb81fd3eed7858718a6f8
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-10 13:56:49 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -7,9 +7,11 @@ if (is_os("windows")) then
 else
 
 add_requires("brew::llvm", {alias = "llvm"})
+add_requires("brew::zlib", {alias = "zlib"})
 
 target("SkrReflector")
     set_kind("binary")
     add_files("**.cpp")
     add_packages("llvm")
+    add_packages("zlib")
 end
